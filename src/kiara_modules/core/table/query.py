b0854b431fbcf1b2dcbd9073c1d3ff782386aa2e
Markus Binsteiner <markus@frkl.io>
2021-07-20 16:18:13 +0200

    feat: add sql query module for arrow table type

--- src/kiara_modules/core/table/query.py
+++ src/kiara_modules/core/table/query.py
@@ -1,9 +1,96 @@
 # -*- coding: utf-8 -*-
 import typing
 
+import duckdb
 import vaex
 from kiara import KiaraModule
 from kiara.data.values import ValueSchema, ValueSet
+from kiara.exceptions import KiaraProcessingException
+from kiara.module_config import KiaraModuleConfig
+from pydantic import Field
+
+RESERVED_SQL_KEYWORDS = [
+    "ALL",
+    "AND",
+    "ARRAY",
+    "AS",
+    "BETWEEN",
+    "BOTH",
+    "CASE",
+    "CHECK",
+    "CONSTRAINT",
+    "CROSS",
+    "CURRENT",
+    "CURRENT",
+    "CURRENT",
+    "CURRENT",
+    "CURRENT",
+    "CURRENT",
+    "DISTINCT",
+    "EXCEPT",
+    "EXISTS",
+    "FALSE",
+    "FETCH",
+    "FILTER",
+    "FOR",
+    "FOREIGN",
+    "FROM",
+    "FULL",
+    "GROUP",
+    "GROUPS",
+    "HAVING",
+    "IF",
+    "ILIKE",
+    "IN",
+    "INNER",
+    "INTERSECT",
+    "INTERSECTS",
+    "INTERVAL",
+    "IS",
+    "JOIN",
+    "LEADING",
+    "LEFT",
+    "LIKE",
+    "LIMIT",
+    "LOCALTIME",
+    "LOCALTIMESTAMP",
+    "MINUS",
+    "NATURAL",
+    "NOT",
+    "NULL",
+    "OFFSET",
+    "ON",
+    "OR",
+    "ORDER",
+    "OVER",
+    "PARTITION",
+    "PRIMARY",
+    "QUALIFY",
+    "RANGE",
+    "REGEXP",
+    "RIGHT",
+    "ROW",
+    "_ROWID",
+    "ROWNUM",
+    "ROWS",
+    "SELECT",
+    "SYSDATE",
+    "SYSTIME",
+    "SYSTIMESTAMP",
+    "TABLE",
+    "TODAY",
+    "TOP",
+    "TRAILING",
+    "TRUE",
+    "UNION",
+    "UNIQUE",
+    "UNKNOWN",
+    "USING",
+    "VALUES",
+    "WHERE",
+    "WINDOW",
+    "WITH",
+]
 
 
 class QueryTableGraphQL(KiaraModule):
@@ -43,6 +130,7 @@ class QueryTableGraphQL(KiaraModule):
             "table": {"type": "table", "doc": "The table to query."},
             "query": {"type": "string", "doc": "The query."},
         }
+
         return inputs
 
     def create_output_schema(
@@ -66,3 +154,71 @@ class QueryTableGraphQL(KiaraModule):
 
         result = df.graphql.execute(query)
         outputs.set_value("query_result", result.to_dict()["data"])
+
+
+class QueryTableSQLModuleConfig(KiaraModuleConfig):
+
+    query: typing.Optional[str] = Field(
+        description="The query to execute. If not specified, the user will be able to provide their own.",
+        default=None,
+    )
+    relation_name: typing.Optional[str] = Field(
+        description="The name the table is referred to in the sql query. If not specified, the user will be able to provide their own.",
+        default=None,
+    )
+
+
+class QueryTableSQL(KiaraModule):
+    """Execute a sql query against an (Arrow) table."""
+
+    _module_type_name = "sql"
+    _config_cls = QueryTableSQLModuleConfig
+
+    def create_input_schema(
+        self,
+    ) -> typing.Mapping[
+        str, typing.Union[ValueSchema, typing.Mapping[str, typing.Any]]
+    ]:
+
+        inputs = {
+            "table": {
+                "type": "table",
+                "doc": "The table to query",
+            }
+        }
+
+        if self.get_config_value("query") is None:
+            inputs["query"] = {"type": "string", "doc": "The query."}
+
+        if self.get_config_value("relation_name") is None:
+            inputs["relation_name"] = {
+                "type": "string",
+                "doc": "The name the table is referred to in the sql query.",
+                "default": "data",
+            }
+
+        return inputs
+
+    def create_output_schema(
+        self,
+    ) -> typing.Mapping[
+        str, typing.Union[ValueSchema, typing.Mapping[str, typing.Any]]
+    ]:
+
+        return {"query_result": {"type": "table", "doc": "The query result."}}
+
+    def process(self, inputs: ValueSet, outputs: ValueSet) -> None:
+
+        _relation_name: str = inputs.get_value_data("relation_name")
+        if _relation_name.upper() in RESERVED_SQL_KEYWORDS:
+            raise KiaraProcessingException(
+                f"Invalid relation name '{_relation_name}': this is a reserved sql keyword, please select a different name."
+            )
+
+        _table = inputs.get_value_data("table")
+        _query = inputs.get_value_data("query")
+
+        relation: duckdb.DuckDBPyRelation = duckdb.arrow(_table)
+        result: duckdb.DuckDBPyResult = relation.query(_relation_name, _query)
+
+        outputs.set_value("query_result", result.arrow())
