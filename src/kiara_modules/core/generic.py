9ae450a93c873bf6152c3888a2a44ab0e62c96e5
Markus Binsteiner <markus@frkl.io>
2021-09-11 21:20:29 +0200

    feat: saving scalars

--- src/kiara_modules/core/generic.py
+++ src/kiara_modules/core/generic.py
@@ -5,9 +5,10 @@ import typing
 import orjson
 from kiara import KiaraModule
 from kiara.data import ValueSet
-from kiara.data.values import ValueSchema
+from kiara.data.values import Value, ValueSchema
 from kiara.exceptions import KiaraProcessingException
-from kiara.operations.save_value import SaveValueModuleConfig
+from kiara.module_config import ModuleTypeConfigSchema
+from kiara.operations.save_value import SaveValueModuleConfig, SaveValueTypeModule
 from pydantic import Field
 
 
@@ -20,6 +21,77 @@ class JsonSerializationConfig(SaveValueModuleConfig):
     file_name: str = Field(
         description="The name of the serialized file.", default="dict.json"
     )
+
+
+class SaveScalarModuleConfig(ModuleTypeConfigSchema):
+
+    value_type: str = Field(description="The value type of the scalar to save.")
+
+
+class SaveBooleanModule(SaveValueTypeModule):
+
+    _module_type_name = "save"
+
+    @classmethod
+    def retrieve_supported_types(cls) -> typing.Union[str, typing.Iterable[str]]:
+        return ["boolean", "integer", "float", "string", "file_path", "folder_path"]
+
+    def save_value(self, value: Value, base_path: str) -> typing.Dict[str, typing.Any]:
+
+        data = value.get_value_data()
+
+        load_config = {
+            "module_type": "generic.load_scalar",
+            "module_config": {"value_type": self.get_config_value("value_type")},
+            "base_path_input_name": None,
+            "inputs": {"scalar_data": data},
+            "output_name": "value_item",
+        }
+
+        return load_config
+
+
+class LoadScalarModuleConfig(ModuleTypeConfigSchema):
+
+    value_type: str = Field(description="The value type of the scalar to load.")
+
+
+class LoadScalarModule(KiaraModule):
+    """Utility module, only used internally."""
+
+    _module_type_name = "load_scalar"
+    _config_cls = LoadScalarModuleConfig
+
+    def create_input_schema(
+        self,
+    ) -> typing.Mapping[
+        str, typing.Union[ValueSchema, typing.Mapping[str, typing.Any]]
+    ]:
+
+        return {
+            "scalar_data": {
+                "type": self.get_config_value("value_type"),
+                "doc": "The scalar value.",
+            }
+        }
+
+    def create_output_schema(
+        self,
+    ) -> typing.Mapping[
+        str, typing.Union[ValueSchema, typing.Mapping[str, typing.Any]]
+    ]:
+
+        return {
+            "value_item": {
+                "type": self.get_config_value("value_type"),
+                "doc": "The loaded item.",
+            }
+        }
+
+    def process(self, inputs: ValueSet, outputs: ValueSet) -> None:
+
+        data = inputs.get_value_obj("scalar_data")
+        outputs.set_value("value_item", data)
 
 
 class LoadFromJsonDictModule(KiaraModule):
